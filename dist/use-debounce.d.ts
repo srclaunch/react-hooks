77520421510dbaf5591f21c225628b3b435fd212
Steven Bennett <steven@budgetbloom.com>
2022-04-28 18:27:13 -0700

    Update gitignore

---
+++ dist/use-debounce.d.ts
@@ -0,0 +1,2 @@
+export declare function useDebounce(value: any, delay: number): any;
+//# sourceMappingURL=use-debounce.d.ts.map
---
+++ dist/use-title.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Update the document title with provided string
+ * @param title is a string.
+ */
+export declare function useTitle(title: string): void;
+//# sourceMappingURL=use-title.d.ts.map
