8bb8648a93be7e71a752746340385289901dfec2
Steven Bennett <steven@budgetbloom.com>
2022-04-29 19:08:57 -0700

    Clean cache

--- dist/use-debounce.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function useDebounce(value: any, delay: number): any;
-//# sourceMappingURL=use-debounce.d.ts.map
--- dist/use-title.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Update the document title with provided string
- * @param title is a string.
- */
-export declare function useTitle(title: string): void;
-//# sourceMappingURL=use-title.d.ts.map
