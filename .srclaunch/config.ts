257be5f9df15084592c50b5c86b6740f3e02e9dd
Steven Bennett <steven@budgetbloom.com>
2022-05-06 13:05:02 -0700

    chore: Clean installation cache

---
+++ .srclaunch/config.ts
@@ -0,0 +1,67 @@
+import {
+  BrowserPackage,
+  BuildFormat,
+  BuildPlatform,
+  BuildTarget,
+  BuildTool,
+  CodeFormatterTool,
+  CodeLinterTool,
+  License,
+  Project,
+  ProjectType,
+  PublishAccess,
+  StaticTypingTool,
+  TestReporter,
+  TestTool,
+} from '@srclaunch/types';
+
+const config: Project = {
+  name: '@srclaunch/react-hooks',
+  description: 'React hooks used in AppLab applications',
+  type: ProjectType.Library,
+  build: {
+    bundle: {
+      exclude: ['react', 'styled-components'],
+      globals: {
+        react: 'React',
+        'styled-components': 'styled',
+      },
+    },
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Node,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+  environments: {
+    development: {
+      formatters: [CodeFormatterTool.Prettier],
+      linters: [CodeLinterTool.ESLint],
+      staticTyping: [StaticTypingTool.TypeScript],
+    },
+  },
+  test: {
+    coverage: {
+      reporters: [TestReporter.Lcov, TestReporter.JSONSummary],
+    },
+    tool: TestTool.Jest,
+  },
+  release: {
+    publish: {
+      access: PublishAccess.Public,
+      license: License.MIT,
+      registry: 'https://registry.npmjs.org/',
+    },
+  },
+  requirements: {
+    node: '>=16',
+    yarn: '>=3.2.0',
+    peerPackages: [BrowserPackage.React],
+    srclaunch: {
+      dx: true,
+      cli: true,
+      types: true,
+    },
+  },
+};
+
+export default config;
